b366e27828588d334a891f92011987e4f6dcba50
austinli246 <74073256+austinli246@users.noreply.github.com>
2021-01-03 20:51:18 -0500

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,22 @@
-function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
-}
+
+  function sumOfMinimumAndMaximum(nums) {
+ 
+  
+    let max = nums[0]; 
+    let min = nums[0];
+    for (let i=0; i<nums.length; i++){
+      if(nums[i]>max)
+        max =nums[i]; 
+    }
+    //max is found
+    for (let i=0; i<nums.length; i++){
+      if (nums[i]<min)
+        min =nums[i]; 
+    }
+    //min is found
+  
+    return max+min;
+  }
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
