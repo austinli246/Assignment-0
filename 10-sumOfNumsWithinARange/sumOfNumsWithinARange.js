6c95027d3c54aa4f7e375f947fa11df2991b9cc5
austinli246 <74073256+austinli246@users.noreply.github.com>
2021-01-03 20:48:32 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,32 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
-}
+  let count =0;
+  let startposition=0;
+  let endposition=0;
+  let foundstart = false;
+  for (i=0;i<nums.length;i++){
+    if (nums[i]===start){
+      startposition = i;
+      foundstart = true;
+    }
+  }
+ let foundend = false;
+  for (i=0;i<nums.length;i++){
+    if (nums[i]===end){
+      endposition = i;
+      foundend =true;
+    }
+  }
 
+//  console.log(startposition); console.log(endposition);
+if (foundstart ===false && foundend ===false)
+return 0;
+if (foundend===false)
+return nums.length;
+else 
+for (i=startposition; i<endposition+1; i++){
+count++;
+}
+return count;
+}
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
