6597db0d3bca252fea342f76e9f2512302b62b3d
austinli246 <74073256+austinli246@users.noreply.github.com>
2021-01-03 21:09:45 -0500

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,12 @@
-function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+function setUnionOfAnyAmountOfSets(...args) {
+  let union = new Set(); 
+
+  for (let i =0; i <args.length; i++){
+    args[i].forEach(union.add,union);
+  }
+
+  return union;
 }
 
 // Do not edit this line;
-module.exports = productOfAnyAmountOfNumbers;
+module.exports = setUnionOfAnyAmountOfSets;
