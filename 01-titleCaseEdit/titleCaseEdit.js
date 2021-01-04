149ea0021d9b1c28e2d4bcf7d60fcfe2b8d2dbbe
austinli246 <74073256+austinli246@users.noreply.github.com>
2021-01-03 20:34:51 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let str = title; // put the sentence into variable str
+  let num = title.split(" "); //split the sentence by space
+  
+  for (let i=0; i<num.length; i++){
+    num[i] = num[i][0].toUpperCase() + num[i].substring(1);
+  }
+  let result= num.join(" "); 
+  return result;
 }
 
 // Do not edit this line;
