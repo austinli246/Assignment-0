3e98d7e65f32fec08c0c61504f59b41453b762e3
austinli246 <74073256+austinli246@users.noreply.github.com>
2021-01-03 20:55:47 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
+
 function frequencyCounter(word) {
-  // Insert code here;
+  
+
+  let arr = {};
+
+  for(var i = 0; i < word.length; i++)
+  {
+   if (arr[word])
+     arr[i]++;
+   else 
+     arr[i] =1; 
+  }
+return arr; 
 }
 
 // Do not edit this line;
