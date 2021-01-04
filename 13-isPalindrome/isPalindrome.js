207740137c461822115763ab67e6ad7d1c5dad89
austinli246 <74073256+austinli246@users.noreply.github.com>
2021-01-03 20:53:18 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let total = word.length; 
+    let a = word.charAt(0);
+    let b = word.charAt(total-1);
+    let c = false;
+    let e = 0;
+    let f =total-1
+    for (let i=0; i<total; i++){
+    if (a===b){
+      a = word.charAt(e++);
+      b = word.charAt(f--); 
+      c= true;
+    }
+    else return false;
+  }
+    return true;
+  
 }
 
 // Do not edit this line;
